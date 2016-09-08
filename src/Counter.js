bb6f75268d5aecb7d591e8a4abca707c6a9c5c8f
Bogdan <info96532@gmail.com>
2016-09-08 15:00:08 +0300

    Redux counter

---
+++ src/Counter.js
@@ -0,0 +1,12 @@
+import React, { Component, PropTypes } from 'react'
+
+const Counter = ({value, onIncrement, onDecrement}) => (
+  <div><h1>{value}</h1>
+  <button onClick={onIncrement}>+</button>
+  <button onClick={onDecrement}>-</button>
+  </div>
+)
+
+
+
+export default Counter
